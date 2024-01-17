437fc78b82b575e7429c644bc82a1363f0947b2b
Yeray Romero <yerayromerodev@gmail.com>
2024-01-17 12:16:07 +0100

    Arregladas consultas sql

--- .sql
+++ .sql
@@ -1,3 +1,3 @@
-SELECT from USER order by ganadas desc; -- Ganadores
-SELECT from USER order by balance asc; -- Perdedores
-SELECT from USER order by saldo desc; -- Saldo
+SELECT * from USER order by ganadas desc; -- Ganadores
+SELECT * from USER order by balance asc; -- Perdedores
+SELECT * from USER order by saldo desc; -- Saldo
