ec35d858f2529fbb7e007facfcb8095e5264deb3
Yeray Romero <yerayromerodev@gmail.com>
2024-01-19 12:23:12 +0100

    Sorteando sorteos

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\LotteryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,14 +10,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(LotteryRepository $lotteryRepository): Response
     {
+        $this->checkLotteries($lotteryRepository);
         return $this->redirectToRoute('app_lottery_index');
     }
 
-    #[Route('/', name: 'app_main')]
+    #[Route('/asasdasda', name: 'app_main_add_money')]
     public function addMoney(): Response
     {
         return $this->redirectToRoute('addMoney.html.twig');
+    }
+    #[Route('/check', name: 'app_check')]
+    public function checkLotteries(LotteryRepository $lotteryRepository)
+    {
+        //$lotteries = $this->getDoctrine()->getRepository(Lottery::class)->findAll();
+        $lotteries = $lotteryRepository->findAll();
+        foreach ($lotteries as $lottery) {
+            if ($lottery->getEndDateTime() < new \DateTime()) {
+                $lottery->setState(1);
+                $lottery->setWinner($lottery->getTickets()[rand(0, $lottery->getStock())]);
+            }
+        }
     }
 }
