5a07efb0925793caf2c42a7ba762a00c2db34518
NuMeRo-999 <pedrovp003@gmail.com>
2024-01-19 11:04:10 +0100

    vista addMoney

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,4 +13,10 @@ class MainController extends AbstractController
     {
         return $this->redirectToRoute('app_lottery_index');
     }
+
+    #[Route('/', name: 'app_main')]
+    public function addMoney(): Response
+    {
+        return $this->redirectToRoute('addMoney.html.twig');
+    }
 }
